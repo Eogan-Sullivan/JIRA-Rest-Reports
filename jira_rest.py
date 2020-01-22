69fe5eb2a93417065bb3f46c739e932844291c80
Eogan Sullivan <eogan.sullivan@outlook.com>
2020-01-22 12:48:38 +0000

    Update jira_rest.py
    Added columns to report + export to csv to specified path

--- jira_rest.py
+++ jira_rest.py
@@ -25,15 +25,34 @@ response = requests.request(
 # Decode Json string to Python
 json_data = json.loads(response.text)
 
+#Add Column Names to Dataframe
+df = pd.DataFrame(columns =['Key','Issue Type', 'Created', 'Reporter','Assignee', 'Status','Task Name', 'Time Spent', 'Original Estimate', 'Time Remaining', 'Time Spent Hours'])
+
+i=2
 # Display issues
-for item in json_data["issues"]:
-    print(item["key"] + "\t" +
-        item["fields"]["issuetype"]["name"] + "\t" +
-        item["fields"]["created"]+ "\t" +
-        item["fields"]["creator"]["displayName"] + "\t" +
-        item["fields"]["status"]["name"] + "\t" +
-        item["fields"]["summary"] + "\t" +
-        str(((item["fields"]["timespent"]/60)/60)) + "\t" +
-        str(item["fields"]["aggregatetimeoriginalestimate"]) + "\t"
-        )
-    
+for item in json_data["issues"]: 
+   
+   assignedTo =""
+   if item["fields"]["assignee"] is None:
+      assignedTo ="Unassigned"
+   else: 
+       assignedTo = item["fields"]["assignee"]["displayName"]
+              
+   df = df.append({'Key' : item["key"],
+          'Issue Type': item["fields"]["issuetype"]["name"],
+          'Created': item["fields"]["created"],
+          'Reporter': item["fields"]["creator"]["displayName"],
+          'Assignee': assignedTo,
+          'Status': item["fields"]["status"]["name"],
+          'Task Name': item["fields"]["summary"],
+          'Time Spent': item["fields"]["timespent"],
+          'Original Estimate' : item["fields"]["aggregatetimeoriginalestimate"],
+          'Time Remaining': "=J"+str(i) +"-I" +str(i),
+          'Time Spent Hours': "=((I"+str(i)+"/60)/60)",
+          'Original Estimate Hours':"=((J"+str(i)+"/60)/60)", 
+          'Time Remaining Hours': "=((K"+str(i)+"/60)/60)",},ignore_index = True)
+   i = i + 1  
+            
+df.to_csv (r'PATH'+str(datetime.now().date())+'.csv')
+print("done")
+
